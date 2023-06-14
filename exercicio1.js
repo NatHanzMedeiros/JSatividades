213a63bbed15e6e2abec7ca549418492d8b8fd94
Nathan Medeiros Cristiano <nathan_cristiano@estudante.sesisenai.org.br>
2023-06-14 20:59:17 -0300

    Add files via upload

---
+++ exercicio1.js
@@ -0,0 +1,11 @@
+function comparar(array){
+    if (array[0] >= array [1]){
+        return true;
+       } 
+        else{
+            return false;
+        } 
+}
+console.log(comparar([5,5]));
+console.log(comparar([5,4]));
+console.log(comparar([4,5]));
---
+++ exercicio3.js
@@ -0,0 +1,61 @@
+function mes(mesnumber){
+if (mesnumber == 1){
+return "Janeiro";    
+}
+if (mesnumber == 2){
+    return "Fevereiro";    
+    }
+if (mesnumber == 3){
+    return "Março";    
+    }
+if (mesnumber == 4){
+    return "Abril";    
+    }
+if (mesnumber == 5){
+    return "Maio";    
+    }
+if (mesnumber == 6){
+    return "Junho";    
+    }
+if (mesnumber == 7){
+    return "Julho";    
+    }
+if (mesnumber == 8){
+    return "Agosto";    
+    }
+if (mesnumber == 9){
+    return "Setembro";    
+    }
+if (mesnumber == 10){
+    return "Outubro";    
+    }
+if (mesnumber == 11){
+    return "Novembro";    
+    }    
+if (mesnumber == 12){
+    return "Dezembro";    
+    }
+
+else if (mesnumber > 12)
+return "Digite um mês valido";
+
+
+}
+console.log(mes(1));
+console.log(mes(2));
+console.log(mes(3));
+console.log(mes(4));
+console.log(mes(5));
+console.log(mes(6));
+console.log(mes(7));
+console.log(mes(8));
+console.log(mes(9));
+console.log(mes(10));
+console.log(mes(11));
+console.log(mes(12));
+console.log(mes(13));
+console.log(mes(14));
+
+
+
+
